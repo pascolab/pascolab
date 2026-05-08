06efb74e960f9c183c093a61a9e092efd673231b
Majid Ali <majidaliofficial129@gmail.com>
2026-05-08 07:56:36 -0700

    fixed build error by removing unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Hero from '@/components/Home/Hero';
 import Brands from '@/components/Home/Brands';
 import Stats from '@/components/SharedComponent/Stats'
 import Services from '@/components/Home/Services';
-import Portfolio from '@/components/SharedComponent/portfollio'
 import Testimonial from '@/components/SharedComponent/Testimonial'
 import Contact from "@/components/Home/Contact";
 import PortfolioShowcase from "@/components/Home/ProjectShowcase";
